44d916d7eac6c0c86ebc1a5e52c79624c9d0d132
joneseyit <joneseyit@protonmail.com>
2018-10-13 21:20:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    return "NO, NOT SINCE 1938!"
-  elsif 
-    return 
+    return  "I LOVE YOU TOO PUMPKIN!"
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938"
   else
     return  "HUH?! SPEAK UP, SONNY!"
   end
